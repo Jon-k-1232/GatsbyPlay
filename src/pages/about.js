a9828af0e454688602c1f4a02eb49e5bbb1d0e55
Jon Kimmel <jonkimmel16@gmail.com>
2020-07-31 23:07:12 -0700

    route

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function about() {
+    return <div>
+        <h1>Hello page 2</h1>
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 import React from "react"
-
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+   <h1>Hello World</h1>
+      <nav>
+            <Link
+                to="/about">
+              About
+            </Link>
+      </nav>
+  </div>
 }
